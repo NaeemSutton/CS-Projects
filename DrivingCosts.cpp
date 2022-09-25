a71a884089168b74fc946f128421ee00ef295d39
NaeemSutton <nsutton@qmail.qcc.edu>
2022-09-25 18:54:55 -0400

    first version

---
+++ DrivingCosts.cpp
@@ -0,0 +1,30 @@
+#include <iomanip>               //For setprecision
+using namespace std;
+
+int main() {
+
+   double milesGallon;
+   double gasGallon;;
+   double cost1;
+   double cost2;
+   double cost3;
+   
+   cin >> milesGallon >> gasGallon;
+   
+   cout << fixed << setprecision(2);
+   
+   cost1 = (20 / milesGallon) * gasGallon;
+   cost2 = (75 / milesGallon) * gasGallon;
+   cost3 = (500 / milesGallon) * gasGallon;
+   
+   cout << cost1;
+   cout << " ";
+   cout << cost2;
+   cout << " ";
+   cout << cost3 << endl;
+   
+   
+   
+
+   return 0;
+}
